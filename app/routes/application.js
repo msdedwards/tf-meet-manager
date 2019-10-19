51cccb6e7ef7736aeb70b16b50b72835bb72dc54
Matthew Edwards <msdedwards@hachisoft.com>
2019-10-19 09:38:05 -0700

    adds pwa configuration

--- app/routes/application.js
+++ app/routes/application.js
@@ -1,7 +1,10 @@
 import Route from '@ember/routing/route';
+import { action } from '@ember/object';
 
 export default class ApplicationRoute extends Route {
-    beforeModel(...args) {
-        this._super(args);
+    @action
+    didTransition() {
+        this.controller.set('isActive', false);
+        return false; // Do not bubble the didTransition event
     }
 }
