6f3e32a4b05eaf307214b043112a2ad79c4772bc
Matthew Edwards <msdedwards@hachisoft.com>
2019-10-19 10:39:20 -0700

    adds icons for pwa optimization

--- config/manifest.js
+++ config/manifest.js
@@ -1,6 +1,6 @@
 'use strict';
 
-module.exports = function(/* environment, appConfig */) {
+module.exports = function (/* environment, appConfig */) {
   // See https://zonkyio.github.io/ember-web-app for a list of
   // supported properties
 
@@ -13,6 +13,16 @@ module.exports = function(/* environment, appConfig */) {
     background_color: "#fff",
     theme_color: "#fff",
     icons: [
+      {
+        src: "./android-chrome-192x192.png",
+        sizes: "192x192",
+        type: "image/png"
+      },
+      {
+        src: "./android-chrome-512x512.png",
+        sizes: "512x512",
+        type: "image/png"
+      }
     ],
     ms: {
       tileColor: '#fff'
